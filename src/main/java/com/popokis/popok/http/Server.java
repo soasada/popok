cf023662ac5f76ab8af41737da6bed54e1478ec2
Nicolas Vargas <nvortega92@gmail.com>
2019-08-14 18:34:04 +0200

    Added license comments to all source files

--- src/main/java/com/popokis/popok/http/Server.java
+++ src/main/java/com/popokis/popok/http/Server.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.popokis.popok.http;
 
 import io.undertow.Handlers;
@@ -45,7 +61,8 @@ public final class Server {
         String httpsPort = appProps.getProperty("server.https.port");
         if (Objects.isNull(httpsPort)) throw new RuntimeException("server.https.port property not found.");
         String keyStorePassword = appProps.getProperty("security.key.store.password");
-        if (Objects.isNull(keyStorePassword)) throw new RuntimeException("security.key.store.password property not found.");
+        if (Objects.isNull(keyStorePassword))
+          throw new RuntimeException("security.key.store.password property not found.");
         if (builder.keyStorePath.isEmpty()) throw new RuntimeException("keyStorePath cannot be empty.");
         this.keyStorePassword = keyStorePassword.toCharArray();
 
