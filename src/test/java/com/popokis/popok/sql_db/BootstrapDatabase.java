1e7b904c63ec17005d37b2b376baa063645d4809
Nicolas Vargas <nvortega92@gmail.com>
2019-10-16 18:09:52 +0200

    Updated h2 and undertow deps. Fixed tests

--- src/test/java/com/popokis/popok/sql_db/BootstrapDatabase.java
+++ src/test/java/com/popokis/popok/sql_db/BootstrapDatabase.java
@@ -23,13 +23,13 @@ public final class BootstrapDatabase {
   private BootstrapDatabase() {}
 
   public static void setUp() {
+    db.executeDML(QueryFactory.create("DROP TABLE IF EXISTS employee"));
     db.executeDML(QueryFactory.create("DROP TABLE IF EXISTS department"));
     db.executeDML(QueryFactory.create("CREATE TABLE department ("
         + "id INTEGER NOT NULL IDENTITY PRIMARY KEY, "
         + "timestamp TIMESTAMP, "
         + "name VARCHAR(255) NOT NULL"
         + ")"));
-    db.executeDML(QueryFactory.create("DROP TABLE IF EXISTS employee"));
     db.executeDML(QueryFactory.create("CREATE TABLE employee ("
         + "id INTEGER NOT NULL IDENTITY PRIMARY KEY, "
         + "name VARCHAR(255) NOT NULL, "
